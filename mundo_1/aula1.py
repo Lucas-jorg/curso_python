440f3440369d1238ed5eee8ac8fce79b0ee32a14
lucas-jorg <lucas7pela@gmail.com>
2024-02-18 19:44:10 -0300

    entrevista

---
+++ mundo_1/aula1.py
@@ -0,0 +1,36 @@
+('ola mundi')
+ModuleNotFoundError
+print ('ola mundo')
+
+entrevista = input ('Vamos te entrevistar Tudo bem?')
+nome = input ('Qual seu nome? ')
+idade = input ('quantos anos voce tem?')
+estado = input (' em qual estado voce mora?')
+bairro = input (' qual bairro reside?')
+motivo = input (' por que devemos te contratar?')
+objetivo = input (' qual seu objetivo de carreira')
+foco = input ('pretende fazer faculdade?')
+doque = input ('qual faculdade?')
+conduçao = input ('quantas condução voce usa da empresa ate sua casa?')
+ok = input (' perfeito ! entraremos em contato contigo.')
+okk = input (' obrigado!!')
+print(entrevista,nome,idade,estado,bairro,motivo,objetivo,foco,doque,conduçao,ok,ook)
+
+#Estrutura condicional Simples (simples tambem pode ser só um if)
+
+# if nome in 'Yara':
+#     print('Prazer Yara, gostei de conhecer voce')
+# else:
+#     print('Não queremos falar com voce! ')
+
+#Estrutura condicional composta 
+
+if nome in "Yara":
+    print('Prazer Yara, gostei de conhecer voce')
+elif nome in "Ingrid":
+    print('Como vai Ingrid?')
+else:
+    nome in "Lucas"
+    print('Prazer Lucas, voce ja esta contratado! Parabens')
+
+
